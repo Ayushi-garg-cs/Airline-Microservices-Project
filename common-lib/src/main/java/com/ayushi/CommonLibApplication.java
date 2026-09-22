51a64d65b2fae64b93e6cebaf59740ca3fb710e8
Ayushi-garg-cs <ayushigarg2346@gmail.com>
2026-09-22 17:33:18 +0530

    Initial commit

---
+++ common-lib/src/main/java/com/ayushi/CommonLibApplication.java
@@ -0,0 +1,4 @@
+package com.ayushi;
+
+public class CommonLibApplication {
+}
---
+++ common-lib/src/main/java/com/ayushi/payload/response/ApiResponse.java
@@ -0,0 +1,8 @@
+package com.ayushi.payload.response;
+
+import lombok.Data;
+
+@Data
+public class ApiResponse {
+    private String message;
+}
